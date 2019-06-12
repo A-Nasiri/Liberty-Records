c5ba447bdbcfc95a69389b17fa2d5b4a18fa0f81
A.Nasiri <50527356+A-Nasiri@users.noreply.github.com>
2019-06-12 19:23:55 +0100

    cartLogic method in UI class
    -clearCart method in UI class to clear all items added to the cart
    -removeItem method in UI class to update cart total, update the local
    storage and update the button text of respective items added or removed

--- app.js
+++ app.js
@@ -136,6 +136,35 @@ class UI {
     cartOverlay.classList.remove("transparentBcg");
     cartDOM.classList.remove("showCart");
   }
+
+  cartLogic() {
+    clearCartBtn.addEventListener("click", () => {
+      this.clearCart();
+    });
+  }
+
+  clearCart() {
+    let cartItems = cart.map(item => item.id);
+    cartItems.forEach(id => this.removeItem(id));
+    while (cartContent.children.length > 0) {
+      cartContent.removeChild(cartContent.children[0]);
+    }
+
+    this.hideCart();
+  }
+
+  removeItem(id) {
+    cart = cart.filter(item => item.id !== id);
+    this.setCartValues(cart);
+    Storage.saveCart(cart);
+    let button = this.getSingleButton(id);
+    button.disabled = false;
+    button.innerHTML = `<i class='fas fa-shopping-cart'></i>Add To Cart`;
+  }
+
+  getSingleButton(id) {
+    return buttonsDOM.find(button => button.dataset.id === id);
+  }
 }
 
 // Local Storage
@@ -175,5 +204,6 @@ document.addEventListener("DOMContentLoaded", () => {
     })
     .then(() => {
       ui.getBagButtons();
+      ui.cartLogic();
     });
 });
